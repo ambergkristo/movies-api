6b94adb8c87bb21f68e898116228a95622039929
kristoamberg <amberg.kristo@gmail.com>
2026-01-09 11:44:44 +0200

    fix: correct movie filtering with genre, actor and year (AND logic)

--- src/main/java/https/gitea/kood/tech/kristoamberg/movies_api/controller/MovieController.java
+++ src/main/java/https/gitea/kood/tech/kristoamberg/movies_api/controller/MovieController.java
@@ -1,15 +1,21 @@
-package https.gitea.kood.tech.kristoamberg.movies_api.controller;
+package https.gitea.kood.tech.kristoamberg.movies_api.repository;
 
 import https.gitea.kood.tech.kristoamberg.movies_api.entity.Movie;
+<<<<<<< HEAD
 import https.gitea.kood.tech.kristoamberg.movies_api.repository.MovieRepository;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+=======
+import org.springframework.data.jpa.repository.JpaRepository;
 
-@RestController
-@RequestMapping("/api/movies")
-public class MovieController {
+import java.util.List;
+import java.util.Optional;
+>>>>>>> 56b9885 (fix: correct movie filtering with genre, actor and year (AND logic))
 
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+<<<<<<< HEAD
     private final MovieRepository movieRepository;
 
     public MovieController(MovieRepository movieRepository) {
@@ -25,4 +31,14 @@ public class MovieController {
     public Movie createMovie(@RequestBody Movie movie) {
         return movieRepository.save(movie);
     }
+=======
+    Optional<Movie> findByTitle(String title);
+
+    // 🔍 FILTERD
+    List<Movie> findByGenres_Id(Long genreId);
+
+    List<Movie> findByActors_Id(Long actorId);
+
+    List<Movie> findByReleaseYear(Integer releaseYear);
+>>>>>>> 56b9885 (fix: correct movie filtering with genre, actor and year (AND logic))
 }
--- src/main/java/https/gitea/kood/tech/kristoamberg/movies_api/repository/MovieRepository.java
+++ src/main/java/https/gitea/kood/tech/kristoamberg/movies_api/repository/MovieRepository.java
@@ -2,6 +2,16 @@ package https.gitea.kood.tech.kristoamberg.movies_api.repository;
 
 import https.gitea.kood.tech.kristoamberg.movies_api.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+<<<<<<< HEAD
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+=======
+import java.util.Optional;
+
+public interface MovieRepository
+        extends JpaRepository<Movie, Long>, JpaSpecificationExecutor<Movie> {
+
+    Optional<Movie> findByTitle(String title);
+>>>>>>> 56b9885 (fix: correct movie filtering with genre, actor and year (AND logic))
 }
